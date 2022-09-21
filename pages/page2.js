8fbfd8221a4c96a52c0f649858c339a0b0e0c92d
okaratop <78306193+okaratop@users.noreply.github.com>
2022-09-21 11:13:06 -0400

    Update page2.js

--- pages/page2.js
+++ pages/page2.js
@@ -15,7 +15,7 @@ export default function Home() {
         <p className="description">
           Nós somos o bytheconteudo! O que fazemos é criar aplicativos para Android, que servem para mostrar o que é possível criar ultilizando apenas o celular. Nenhum aplicativo é vendido, alguns projetos são liberados para download para serem usados no Sketchware. Obrigado por acompanhar!
         </p>
-        <a href="https://wipsites.com.br"><button style="background: #069cc2; border-radius: 6px; padding: 15px; cursor: pointer; color: #fff; border: none; font-size: 16px;">Voltar</button></a>
+        <a href="https://lively-gnome-333ad2.netlify.app/"><button style="background: #069cc2; border-radius: 6px; padding: 15px; cursor: pointer; color: #fff; border: none; font-size: 16px;">Voltar</button></a>
       </main>
 
       <Footer />
